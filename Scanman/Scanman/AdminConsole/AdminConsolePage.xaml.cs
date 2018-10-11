2c7c97ec73b6094e2fa2bd29907af042eeb36532
KingToasty <julian.palmieri.business@gmail.com>
2018-10-11 19:19:16 +1100

    Add files via upload

---
+++ Scanman/Scanman/AdminConsole/AdminConsolePage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Scanman
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class AdminConsolePage : Page
+    {
+        public AdminConsolePage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void NavView_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (NavigationViewItemBase item in NavView.MenuItems)
+            {
+                if (item is NavigationViewItem && item.Tag.ToString() == "home")
+                {
+                    NavView.SelectedItem = item;
+                    break;
+                }
+            }
+        }
+
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            var item = sender.MenuItems.OfType<NavigationViewItem>().First(x => (string)x.Content == (string)args.InvokedItem);
+            NavView_Navigate(item as NavigationViewItem);
+        }
+
+        private void NavView_Navigate(NavigationViewItem item)
+        {
+            switch (item.Tag)
+            {
+                case "home":
+                    ContentFrame.Navigate(typeof(ConsoleHome));
+                    break;
+                case "students":
+                    ContentFrame.Navigate(typeof(StudentsPage));
+                    break;
+                case "judo":
+                    ContentFrame.Navigate(typeof(JudoPage));
+                    break;
+                case "jiu-jitsu":
+                    ContentFrame.Navigate(typeof(JiuJitsuPage));
+                    break;
+            }
+        }
+
+        private void ContentFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+
+        }
+    }
+}
